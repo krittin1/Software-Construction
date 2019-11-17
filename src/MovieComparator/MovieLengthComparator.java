f1f59ae959a831900e95e48449e2d371536a4a44
Arty                Kanok <krittin.kan1@ku.th>
2019-11-18 00:39:25 +0700

    Add files via upload

---
+++ src/MovieComparator/MovieLengthComparator.java
@@ -0,0 +1,12 @@
+package tutorial;
+
+import java.util.Comparator;
+
+public class MovieLengthComparator implements Comparator<Movie> {
+
+
+    @Override
+    public int compare(Movie o1, Movie o2) {
+        return o1.getLength()-o2.getLength();
+    }
+}
---
+++ src/MovieComparator/MovieRatingScoreComparator.java
@@ -0,0 +1,18 @@
+package tutorial;
+
+import java.util.Comparator;
+
+public class MovieRatingScoreComparator implements Comparator<Movie> {
+
+    @Override
+    public int compare(Movie o1, Movie o2) {
+
+        if (o1.getRatingScore() > o2.getRatingScore()){
+            return -1;
+        }
+        else if (o1.getRatingScore() < o2.getRatingScore()){
+            return 1;
+        }
+        return 0;
+    }
+}
